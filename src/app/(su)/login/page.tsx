98aca9fafb0302d4dfb6a9c089089d5a4bbb2f0f
Nikhil Kumar <nknapster@gmail.com>
2025-03-25 15:35:17 +0530

    update login form from shadcn blocks

--- src/app/(su)/login/page.tsx
+++ src/app/(su)/login/page.tsx
@@ -1,42 +1,21 @@
-import React from "react";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
+import { LoginForm } from "@/components/login-form";
 import { login } from "../actions";
-export default function LoginPage() {
+
+export default function Page() {
   return (
-    <div className="mx-12">
-      <form
-        action={async (formdata) => {
-          "use server";
-          const username = formdata.get("username") as string;
-          const password = formdata.get("password") as string;
-          console.log(username, password);
-          await login(username, password);
-        }}
-      >
-        <Card>
-          <CardHeader>
-            <CardTitle>Login</CardTitle>
-            <CardDescription>Only for administrative use</CardDescription>
-          </CardHeader>
-          <CardContent className="flex flex-col gap-4">
-            <Input name="username" type="text" placeholder="Username" />
-            <Input name="password" type="password" placeholder="Password" />
-          </CardContent>
-          <CardFooter>
-            <Button>Log in</Button>
-          </CardFooter>
-        </Card>
-      </form>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm
+          action={async (formdata) => {
+            "use server";
+            console.log(formdata);
+            const username = formdata.get("username") as string;
+            const password = formdata.get("password") as string;
+            console.log(username, password);
+            await login(username, password);
+          }}
+        />
+      </div>
     </div>
   );
 }
