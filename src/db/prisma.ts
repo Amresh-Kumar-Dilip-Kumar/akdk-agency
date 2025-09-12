a2970772e49e5661d780d1d24baa992c227c46c7
nikhil kumar <nknapster@gmail.com>
2025-09-12 09:41:04 +0000

    updated prisma to use generate folder output and client form /generated... fixed:delete message auto closing

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,5 +1,5 @@
 import 'server-only';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@/../generated';
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
